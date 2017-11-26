eecfee6bd2bb64d8da14f6581d9e9bb125aef80e
asd <asd@DESKTOP-MOKD2JG>
2017-11-26 16:50:53 -0600

    Navegacion

---
+++ Bases/Bases/Principal.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bases
+{
+    /// <summary>
+    /// Interaction logic for Principal.xaml
+    /// </summary>
+    public partial class Principal : Page
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Clientes_Click(object sender, RoutedEventArgs e)
+        {
+            Clientes paginaClientes = new Clientes();
+            this.NavigationService.Navigate(paginaClientes);
+        }
+    }
+}
